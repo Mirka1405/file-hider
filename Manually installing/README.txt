a82ca5b34500ef013d9f26f921ccd3a1ec204a34
Mirka1405 <96072040+Mirka1405@users.noreply.github.com>
2022-03-08 14:47:43 +0300

    Manual installation

---
+++ Manually installing/README.txt
@@ -0,0 +1,8 @@
+Hello! Glad you opened this.
+contents.txt is storing all the files that are hidden with no security,
+but the way it's hidden is pretty unknown to most people, so nobody will find the files.
+It is still secure, since you can't have alternate data streams in an USB drive because it stores files differently.
+Your files are safe, unless you have AlternateDataStreamView or something like that on your PC.
+You can't change the names of the folders, that will make them inaccessible.
+Ironically, you can change the content of contents.txt, but not its name.
+You can delete this README file now.
---
+++ Manually installing/hide.py
@@ -0,0 +1,129 @@
+from ctypes import *
+
+import sys, os
+kernel32 = windll.kernel32
+
+LPSTR     = c_wchar_p
+DWORD     = c_ulong
+LONG      = c_ulong
+WCHAR     = c_wchar * 296
+LONGLONG  = c_longlong
+
+class LARGE_INTEGER_UNION(Structure):
+    _fields_ = [
+        ("LowPart", DWORD),
+        ("HighPart", LONG),]
+
+class LARGE_INTEGER(Union):
+    _fields_ = [
+        ("large1", LARGE_INTEGER_UNION),
+        ("large2", LARGE_INTEGER_UNION),
+        ("QuadPart",    LONGLONG),
+    ]
+
+class WIN32_FIND_STREAM_DATA(Structure):
+    _fields_ = [
+        ("StreamSize", LARGE_INTEGER),
+        ("cStreamName", WCHAR),
+    ]
+    '''
+    typedef struct _WIN32_FIND_STREAM_DATA {
+      LARGE_INTEGER StreamSize;
+      WCHAR         cStreamName[MAX_PATH + 36];
+    } WIN32_FIND_STREAM_DATA, *PWIN32_FIND_STREAM_DATA;
+    '''
+
+class ADS():
+    def __init__(self, filename):
+        self.filename = filename
+        self.streams = self.init_streams()
+
+    def init_streams(self):
+        file_infos = WIN32_FIND_STREAM_DATA()
+        streamlist = list()
+
+        findFirstStreamW = kernel32.FindFirstStreamW
+        findFirstStreamW.restype = c_void_p
+
+        myhandler = kernel32.FindFirstStreamW (LPSTR(self.filename), 0, byref(file_infos), 0)
+        '''
+        HANDLE WINAPI FindFirstStreamW(
+          __in        LPCWSTR lpFileName,
+          __in        STREAM_INFO_LEVELS InfoLevel, (0 standard, 1 max infos)
+          __out       LPVOID lpFindStreamData, (return information about file in a WIN32_FIND_STREAM_DATA if 0 is given in infos_level
+          __reserved  DWORD dwFlags (Reserved for future use. This parameter must be zero.) cf: doc
+        );
+        https://msdn.microsoft.com/en-us/library/aa364424(v=vs.85).aspx
+        '''
+        p = c_void_p(myhandler)
+
+        if file_infos.cStreamName:
+            streamname = file_infos.cStreamName.split(":")[1]
+            if streamname: streamlist.append(streamname)
+
+            while kernel32.FindNextStreamW(p, byref(file_infos)):
+                streamlist.append(file_infos.cStreamName.split(":")[1])
+
+        kernel32.FindClose(p)  # Close the handle
+
+        return streamlist
+
+    def __iter__(self):
+        return iter(self.streams)
+
+    def has_streams(self):
+        return len(self.streams) > 0
+
+    def full_filename(self, stream):
+        return "%s:%s" % (self.filename, stream)
+
+    def add_stream_from_file(self, filename):
+        if os.path.exists(filename):
+            with open(filename, "rb") as f:
+                content = f.read()
+            return self.add_stream_from_string(filename, content)
+        else:
+            print("Could not find file: {0}".format(filename))
+            return False
+
+    def add_stream_from_string(self, stream_name, string):
+        fullname = self.full_filename(os.path.basename(stream_name))
+        if os.path.exists(fullname):
+            print("Stream name already exists")
+            return False
+        else:
+            fd = open(fullname, "wb")
+            fd.write(string)
+            fd.close()
+            self.streams.append(stream_name)
+            return True
+
+    def delete_stream(self, stream):
+        try:
+            os.remove(self.full_filename(stream))
+            self.streams.remove(stream)
+            return True
+        except:
+            return False
+
+    def get_stream_content(self, stream):
+        fd = open(self.full_filename(stream), "rb")
+        content = fd.read()
+        fd.close()
+        return content
+
+        
+from os import listdir,fsencode,remove
+from os.path import dirname,realpath
+from time import sleep
+p = dirname(realpath(__file__))
+directory = fsencode(p+"\\hidden folder")
+a = ADS(p+"\\hidden folder\\contents.txt")
+for file in listdir(directory):
+    if file == b"contents.txt":     continue
+    file = file.decode("utf-8")
+    a.add_stream_from_file(p+"\\hidden folder\\"+file)
+    print(f"Hiding {file}...")
+    remove(p+f"\\hidden folder\\{file}")
+print("Hidden successfully!")
+sleep(5)
